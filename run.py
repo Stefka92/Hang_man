6291573698e7fe2995cfe735d44f4547633f5eab
Stephanie McMahon <Smm200973@hotmail.com>
2023-02-08 17:49:17 +0000

    add function that creates a random word

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random 
+from words import word_list
+
+def get_word ():
+    word = random.choices(word_list)
+    return.word.upper()
