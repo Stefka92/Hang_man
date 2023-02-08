68b75d71afa727e0d2bb3d786b34b7d345eacdc4
Stephanie McMahon <Smm200973@hotmail.com>
2023-02-08 18:24:07 +0000

    add main function to play game

--- run.py
+++ run.py
@@ -1,6 +1,27 @@
 import random 
-from words import word_list
+from words import random_words
 
-def get_word ():
-    word = random.choices(word_list)
-    return.word.upper()
+
+def get_words():
+    """
+    This function generates a 
+    random word for the user to guess.  
+    """
+    word = random.choices(random_words)
+    return word.upper()
+
+def play_game(words):
+    """
+    This function operates the main game,
+    it allows the user 6 incorrect guesses before the man is hung.
+    """
+    completed_word = ["_"] * len(words)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    print("MY life depends on you.... No pressure\n")
+    print(display_hangman_lives(tries))
+    print(completed_word)
+    
+
