42d31466fcf6036147329b36572fca5af6a6b7bf
Stephanie McMahon <Smm200973@hotmail.com>
2023-02-11 12:30:22 +0000

    create function that breaks out of loop if correct word is guessed

--- run.py
+++ run.py
@@ -26,4 +26,29 @@ def hangman(words):
     win = False
     print("Welcome to Hangman, My life depends on you...\N")   # displaying the message to the user 
     guesses = []
+    while wrong < len(stages) - 1:   # if the user guess is less than -1 then 
+        print("\n")
+        msg = "Please guess a letter: "   # asking the user to guess a letter 
+        char = input(msg)   # getting the guessed letter as a input 
+        if char in guesses:   # if the entered char matches with the word then, 
+            print("Whoops you already guessed the letter '{}', try again.".format(char))
+            continue
+
+        guesses.append(char)
+        if char in rletters:  # if the character in the rletters then 
+            cind = rletters.index(char) 
+            board[cind] = char
+            rletters[cind] = '$'
+        else:  # if the character is not in the rletters then 
+            wrong += 1   # update the wrong value by 1 
+        print((" ".join(board))
+        print("Attempts remaining: {}/{}".format(len(stages) - 1 - wrong, len(stages) - 1))   # printing the attempts remaining
+        print("Previous Guesses: {}".format(guesses))  # displaying the Previous guesses by the user 
+        e = wrong + 1
+        print("\n".join(stages[0: e]))
+        if "_" not in board:  # if _ is not in board then print 
+            print("You win!")  # telling to the user that he or she won 
+            print(" ".join(board))
+            win = True
+            break
     
