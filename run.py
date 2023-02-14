346f6f2f31168b323d0051a59b4f61e355dc3dec
Stephanie McMahon <Smm200973@hotmail.com>
2023-02-14 23:20:10 +0000

    clean up unnecessary whitespace

--- run.py
+++ run.py
@@ -100,10 +100,10 @@ def hangman(words):
     hint_used = False
     while wrong < len(stages) - 1:   # if the user guess is less than -1 then 
         print("\n")
-        msg = (Fore.YELLOW + "Please guess a letter, type 'hint' for a hint, or guess the word: "+ Style.RESET_ALL)
+        msg = (Fore.YELLOW + "Please guess a letter, type 'hint' for a hint, or guess the word: " + Style.RESET_ALL)
         char = input(msg)   # getting the guessed letter as a input
 
-        if char == "hint": # provides a hint to the user
+        if char == "hint":  # provides a hint to the user
             if not hint_used:
                 hint = [l for l in rletters if l not in guesses]
                 print("Here's a hint: one of the letters not yet guessed is '{}'".format(hint[0]))
@@ -118,12 +118,12 @@ def hangman(words):
             wrong += 1
             continue
 
-        if char == words: # if you guess the correct word
+        if char == words:  # if you guess the correct word
             print("congratulations! You guessed correctly")
             win = True
             break
 
-        if not char.isalpha(): # if the entered char is not a letter
+        if not char.isalpha():  # if the entered char is not a letter
             print("Invalid input. Please enter a single letter, the word, or 'hint'.")
             continue
 
@@ -144,10 +144,10 @@ def hangman(words):
             wrong += 1   # update the wrong value by 1 
         print((" ".join(board)))
         print("Attempts remaining: {}/{}".format(len(stages) - 1 - wrong, len(stages) - 1))   # printing the attempts remaining
-        print(Fore.RED +"Previous Guesses: {}".format(guesses)+ Style.RESET_ALL)  # displaying the Previous guesses by the user 
+        print(Fore.RED + "Previous Guesses: {}".format(guesses) + Style.RESET_ALL)  # displaying the Previous guesses by the user 
         e = wrong + 1
         print("\n".join(stages[0: e]))
-        if "_" not in board and wrong < len(stages) -1:  # if _ is not in board and number of attemps not exceeded
+        if "_" not in board and wrong < len(stages) - 1:  # if _ is not in board and number of attemps not exceeded
             print("You win!")  # telling to the user that they won 
             print(" ".join(board))
             win = True
