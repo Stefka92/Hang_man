db67d0a15c8b815746bcf0a2dc42435cbafbef3e
Stephanie McMahon <Smm200973@hotmail.com>
2023-02-23 22:12:36 +0000

    fix indintation error

--- run.py
+++ run.py
@@ -158,9 +158,9 @@ def hangman(words, stages):
             win = True
             break
 
-        if not win:  # if out of guesses show correct answer
-            print("\n".join(stages[0: wrong]))
-            print("You lose! The word was {}.".format(words))
+    if not win:  # if out of guesses show correct answer
+        print("\n".join(stages[0: wrong]))
+        print("You lose! The word was {}.".format(words))
 
 
 def play_again():
